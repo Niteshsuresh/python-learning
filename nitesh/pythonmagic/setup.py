bc1dfd1bc7f9fcd7f1acdca485908b2755df1d50
Niteshsuresh <nitesh@pratian.com>
2016-12-22 09:45:09 +0530

    initializing the repo for magic

---
+++ nitesh/pythonmagic/setup.py
@@ -0,0 +1,9 @@
+setup(name='pythonmagic',
+      version='0.1',
+      description='Library to match the word with given letters',
+      url='http://github.com/niteshsuresh/nitesh/magic-python',
+      author='NiteshSuresh',
+      author_email='niteshsuresh',
+      license='Proprietary',
+      packages=['pip','pep8']
+    )
